b50aa32996c94357c930373838cce936a20aabef
Mark Shteyn <markshteyn1@gmail.com>
2026-01-26 10:13:16 -0500

    Create Reef/Secrets.swift

---
+++ Reef/Secrets.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+/// API keys and secrets - DO NOT commit this file to version control
+enum Secrets {
+    static let tokenCompanyAPIKey = "ttc_sk_fMp6VhPqMml42dqWx68Tf92kuT6m7mT7YT52ggZe_Jc"
+}
