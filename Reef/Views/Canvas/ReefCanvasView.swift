0ef42fbbfa8384c11b3c3bc093cae86c024d862c
Mark Shteyn <markshteyn1@gmail.com>
2026-01-26 10:43:31 -0500

    Update Reef/Views/Canvas/ReefCanvasView.swift

--- Reef/Views/Canvas/ReefCanvasView.swift
+++ Reef/Views/Canvas/ReefCanvasView.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ReefCanvasView: PKCanvasView {
 
+    // MARK: - Selection State
+
+    var onSelectionChanged: ((Bool) -> Void)?
+
+    var hasSelection: Bool {
+        canPerformAction(#selector(copy(_:)), withSender: nil)
+    }
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        let result = super.canPerformAction(action, withSender: sender)
+
+        // Notify when selection-related actions change
+        if action == #selector(copy(_:)) || action == #selector(cut(_:)) || action == #selector(delete(_:)) {
+            DispatchQueue.main.async { [weak self] in
+                self?.onSelectionChanged?(self?.hasSelection ?? false)
+            }
+        }
+
+        return result
+    }
+
     // MARK: - Clipboard Operations
 
     @objc override func paste(_ sender: Any?) {
